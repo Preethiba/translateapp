3c8a18539fc2c298f85b89d9badc66a429d950df
Preethiba <ibpreethiba99@gmail.com>
2021-05-04 09:09:02 +0530

    1 file modified, 4 added, 1 deleted

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,30 @@
 import React from "react";
+import UserCreate from "./UserCreate";
 
 class App extends React.Component {
-  state = { language: "English" };
+  state = { language: "hindi" };
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
 
   render() {
-    return <div class="ui container">App</div>;
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i
+            className="flag in"
+            onClick={() => this.onLanguageChange("hindi")}
+          />
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange("english")}
+          />
+        </div>
+        <UserCreate />
+      </div>
+    );
   }
 }
 export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Button extends React.Component {
+  render() {
+    return <Button className="ui button primary" />;
+  }
+}
+
+export default Button;
