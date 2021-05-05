d07cb833a0ae726add1f8d2d095941c925e006e1
Preethiba <ibpreethiba99@gmail.com>
2021-05-05 09:36:13 +0530

    2 files modified, 1 added, 1 deleted

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import UserCreate from "./UserCreate";
 import LanguageContext from "../context/LanguageContext";
+import ColorContext from "../context/ColorContext";
 
 class App extends React.Component {
+  state = { language: "hindi" };
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
+
   render() {
     return (
       <div className="ui container">
@@ -13,11 +20,15 @@ class App extends React.Component {
             onClick={() => this.onLanguageChange("english")}
           />
           <i
-            className="flag nl"
+            className="flag du"
             onClick={() => this.onLanguageChange("dutch")}
           />
         </div>
-        <UserCreate />
+        <LanguageContext.Provider value={this.state.language}>
+          <ColorContext.Provider value="red">
+            <UserCreate />
+          </ColorContext.Provider>
+        </LanguageContext.Provider>
       </div>
     );
   }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,30 @@
 import React from "react";
 import LanguageContext from "../context/LanguageContext";
+import ColorContext from "../context/ColorContext";
 
 class Button extends React.Component {
-  static contextType = LanguageContext; //to access context inside class using contextType, adds a context prop to the class Button
+  // static contextType = LanguageContext; //to access context inside class using contextType, adds a context prop to the class Button
+
+  renderButton(color) {
+    return (
+      <Button className={`ui button ${color}`}>
+        <LanguageContext.Consumer>
+          {value => (value === "english" ? "Submit" : "Voorlegen")}{" "}
+          {/*  provided as child to consumer component and it is automatically invoked , value is the value present in context */}
+        </LanguageContext.Consumer>
+      </Button>
+    );
+  }
 
   render() {
-    const text = this.context === "english" ? "Submit" : "Voorlegen";
-    return <Button className="ui button primary">{text}</Button>;
+    // const text = this.context === "english" ? "Submit" : "Voorlegen";
+    return (
+      <ColorContext.Consumer>
+        {color => {
+          this.renderButton;
+        }}
+      </ColorContext.Consumer>
+    );
   }
 }
 
